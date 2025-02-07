0ae451f472c983e9080e879f63f75233106b7959
DyakivNazar <nazar2005.03.21@gmail.com>
2025-02-07 15:46:08 +0200

    question the form

---
+++ JS/форма/hw2/main.js
@@ -0,0 +1,8 @@
+//iBvqtjEm
+console.log('Task 6');
+let v;
+v = 'wewefwefwef';
+if (v === undefined || isNaN(v) || v === null || v === false || v === 0 || v === '') {
+    v = 'default';
+}
+console.log(v);
