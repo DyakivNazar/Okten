07d28a7e2649de1fe2b4e10e5da70d6b5ebeef0f
DyakivNazar <nazar2005.03.21@gmail.com>
2025-03-10 20:33:58 +0200

    Add HW11

---
+++ JS/Homework11/carts/main.js
@@ -0,0 +1,67 @@
+fetch('https://dummyjson.com/carts?limit=50')
+    .then(value => value.json())
+    .then(res => {
+        let {carts} = res;
+        console.log(carts);
+
+        let wrapp_carts = document.getElementById('wrapp_carts');
+
+        for (const cart of carts) {
+            let div_cart = document.createElement('div');
+
+            let h3 = document.createElement('h3');
+            h3.innerHTML = 'Корзина ' + cart.id;
+
+            let div_product = document.createElement('div');
+            div_product.className = 'div_product';
+
+            for (let product of cart.products) {
+
+                let div_product_wrapp = document.createElement('div');
+
+                let h4_title = document.createElement('h4');
+                h4_title.innerHTML = 'title: ' + product.title;
+
+                let p_price = document.createElement('p');
+                p_price.innerHTML = 'price ' + product.price;
+
+                let p_quantity = document.createElement('p');
+                p_quantity.innerHTML = 'quantity: ' + product.quantity;
+
+                let p_total = document.createElement('p');
+                p_total.innerHTML = 'total: ' + product.total;
+
+                let p_discountPercentage = document.createElement('p');
+                p_discountPercentage.innerHTML = 'discountPercentage: ' + product.discountPercentage;
+
+                let p_discountedTotal = document.createElement('p');
+                p_discountedTotal.innerHTML = 'discountedTotal: ' + product.discountedTotal;
+
+                let img = document.createElement('img');
+                img.src = product.thumbnail;
+                img.alt = 'img product ' + product.title;
+
+                div_product_wrapp.append(h4_title, p_price, p_quantity, p_total, p_discountPercentage, p_discountedTotal, img);
+                div_product.appendChild(div_product_wrapp);
+            }
+
+            let p_total = document.createElement('p');
+            p_total.innerHTML = 'total: ' + cart.total;
+
+            let p_discountedTotal = document.createElement('p');
+            p_discountedTotal.innerHTML = 'discountedTotal: ' + cart.discountedTotal;
+
+            let p_user_id = document.createElement('p');
+            p_user_id.innerHTML = 'user_id: ' + cart.userId;
+
+            let p_totalProducts = document.createElement('p');
+            p_totalProducts.innerHTML = 'totalProducts: ' + cart.totalProducts;
+
+            let p_totalQuantity = document.createElement('p');
+            p_totalQuantity.innerHTML = 'totalQuantity: ' + cart.totalQuantity;
+
+            div_cart.append(h3, div_product, p_total, p_discountedTotal, p_user_id, p_totalProducts, p_totalQuantity);
+            wrapp_carts.appendChild(div_cart);
+        }
+    });
+
---
+++ JS/Homework11/recipes/main.js
@@ -0,0 +1,87 @@
+fetch('https://dummyjson.com/recipes?limit=50')
+    .then(value => value.json())
+    .then(res => {
+        let {recipes} = res;
+        console.log(recipes)
+
+        let wrapp_recipes = document.getElementById('wrapp_recipes');
+
+        for (const recipe of recipes) {
+            let div_recipe = document.createElement('div');
+
+            let h3 = document.createElement('h3');
+            h3.className = 'h3_recipe';
+            h3.innerHTML = 'Рецепт ' + recipe.id;
+
+            let h4_name = document.createElement('h4');
+            h4_name.innerHTML = `Name recipe <span style="font-weight: normal;">${recipe.name}</span>`;
+
+            let h3_ingredient = document.createElement('h3');
+            h3_ingredient.innerHTML = 'Ingredient'
+            let ul_ingredient = document.createElement('ul');
+
+            for (const ingredient of recipe.ingredients) {
+                let li_ingredient = document.createElement('li');
+                for (let ingredient_li of ingredient) {
+                    li_ingredient.append(ingredient_li);
+                }
+                ul_ingredient.appendChild(li_ingredient);
+            }
+
+            let div_instruction = document.createElement('div');
+            let h3_instruction = document.createElement('h3');
+            h3_instruction.innerHTML = 'Instruction';
+
+            for (const instruction of recipe.instructions) {
+                let p_instruction = document.createElement('p');
+
+                p_instruction.append(instruction);
+                div_instruction.append(p_instruction);
+            }
+
+            let div_text = document.createElement('div');
+
+            let p_prepTimeMinutes = document.createElement('p');
+            p_prepTimeMinutes.innerHTML = '<b>prepTimeMinutes:</b> ' + recipe.prepTimeMinutes;
+
+            let p_cookTimeMinutes = document.createElement('p');
+            p_cookTimeMinutes.innerHTML = '<b>cookTimeMinutes:</b> ' + recipe.cookTimeMinutes;
+
+            let p_servings = document.createElement('p');
+            p_servings.innerHTML = '<b>servings:</b> ' + recipe.servings;
+
+            let p_difficulty = document.createElement('p');
+            p_difficulty.innerHTML = '<b>difficulty:</b> ' + recipe.difficulty;
+
+            let p_cuisine = document.createElement('p');
+            p_cuisine.innerHTML = '<b>cuisine:</b> ' + recipe.cuisine;
+
+            let p_caloriesPerServing = document.createElement('p');
+            p_caloriesPerServing.innerHTML = '<b>caloriesPerServing:</b> ' + recipe.caloriesPerServing
+
+            let p_tags = document.createElement('p');
+            p_tags.innerHTML = '<b>tags:</b> ' + recipe.tags.join(", ");
+
+            let p_userId = document.createElement('p');
+            p_userId.innerHTML = '<b>userId:</b> ' + recipe.userId;
+
+            let img = document.createElement('img');
+            img.src = recipe.image;
+            img.alt = recipe.name;
+
+            let p_rating = document.createElement('p');
+            p_rating.innerHTML = '<b>rating:</b> ' + recipe.rating
+
+            let p_reviewCount = document.createElement('p');
+            p_reviewCount.innerHTML = '<b>reviewCount:</b> ' + recipe.reviewCount
+
+            let p_mealType = document.createElement('p');
+            p_mealType.innerHTML = '<b>mealType:</b> ' + recipe.mealType.join(", ")
+
+            div_text.append(p_prepTimeMinutes, p_cookTimeMinutes, p_servings, p_difficulty, p_cuisine, p_caloriesPerServing, p_tags, p_userId, p_rating, p_reviewCount, p_mealType)
+
+            div_recipe.append(h3, h4_name, img, h3_ingredient, ul_ingredient, h3_instruction, div_instruction, div_text);
+            wrapp_recipes.appendChild(div_recipe);
+
+        }
+    });
