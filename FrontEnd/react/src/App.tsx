7a54b4ab967e43ec1d9a9a35e959412bebc86d1e
DyakivNazar <nazar2005.03.21@gmail.com>
2025-04-05 00:28:50 +0300

    hw1 #kLZQR7Q98

--- FrontEnd/react/src/App.tsx
+++ FrontEnd/react/src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import {simpsons} from "./data/simpsons.ts";
+import Mysimpsons from "./components/my-simpsons/Mysimpsons.tsx";
 
 function App() {
 
   return (
     <>
-
+      {
+        simpsons.map((simpson, index) => <Mysimpsons simpson={simpson} key={index} /> )
+      }
     </>
   )
 }
