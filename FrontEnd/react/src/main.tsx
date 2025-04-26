cc6a5de2159e30410e1b81469f4c19fe48d32d7d
DyakivNazar <nazar2005.03.21@gmail.com>
2025-04-26 19:46:14 +0300

    appdate rep main, add react-router-dom

--- FrontEnd/react/src/main.tsx
+++ FrontEnd/react/src/main.tsx
@@ -1,5 +1,13 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import MainLayout from './layouts/MainLayout';
 
-createRoot(document.getElementById('root')!).render(<App/>)
+const router = createBrowserRouter(
+    [
+        {
+            path:'/', element:<MainLayout/>,
+        }
+    ]
+)
+createRoot(document.getElementById('root')!).render(<RouterProvider router={router}/>)
